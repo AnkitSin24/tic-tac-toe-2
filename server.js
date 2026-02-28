d5fde80a066e2d83d359e6c69da07e3e36f38f61
Ankit Singh <89828727+AnkitSin24@users.noreply.github.com>
2026-02-28 14:17:20 +0530

    tictactoe server

--- server.js
+++ server.js
@@ -127,6 +127,12 @@ io.to(data.room).emit("chat",data)
 
 })
 
-http.listen(3000,()=>{
-console.log("Server running http://localhost:3000")
+const PORT = process.env.PORT || 5000
+
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
 })
+
+// http.listen(5000,()=>{
+// console.log("Server running http://localhost:5000")
+// })
